754e655872ae17ebf17ab4ec671f0f02159c251c
Behnam Reyhani-Masoleh <benreyhani15@hotmail.com>
2015-10-12 20:57:33 -0700

    added OCR library

---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/behnamreyhani-masoleh/Documents/Android/NightOut/nightout/tess/tess-two/bin/AndroidManifest.xml : \
+/Users/behnamreyhani-masoleh/Documents/Android/NightOut/nightout/tess/tess-two/AndroidManifest.xml \
