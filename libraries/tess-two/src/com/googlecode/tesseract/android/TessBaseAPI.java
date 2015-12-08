f25b46c3aa28dedc2af3d531ae7be5402eecdf6f
Behnam Reyhani-Masoleh <benreyhani15@hotmail.com>
2015-12-08 10:36:56 -0800

    hot fix for loading tess library error

--- libraries/tess-two/src/com/googlecode/tesseract/android/TessBaseAPI.java
+++ libraries/tess-two/src/com/googlecode/tesseract/android/TessBaseAPI.java
@@ -40,7 +40,7 @@ public class TessBaseAPI {
     private long mNativeData;
 
     static {
-        System.loadLibrary("pngt");
+        System.loadLibrary("png");
         System.loadLibrary("lept");
         System.loadLibrary("tess");
 
