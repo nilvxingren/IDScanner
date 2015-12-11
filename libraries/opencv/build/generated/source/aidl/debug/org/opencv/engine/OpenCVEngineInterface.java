e607e757db6ffd1ddee46b53fc8b90415ed72eae
Mohammed Babur <mbabur@nvidia.com>
2015-12-11 14:47:19 -0600

    *.class added to .gitignore

--- libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ libraries/opencv/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/behnamreyhani-masoleh/Documents/Android/NightOut/nightout/IDScannerTest/libraries/opencv/src/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\mbabur.NVIDIA.COM\\Desktop\\id_scanner\\libraries\\opencv\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
