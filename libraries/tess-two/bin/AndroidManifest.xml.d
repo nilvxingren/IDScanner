8b5ac44cef8d4a8a0d2fe4c1a27b5b915d6d56e3
Behnam Reyhani-Masoleh <benreyhani15@hotmail.com>
2015-12-11 14:12:15 -0800

    fixed .gitignore

--- libraries/tess-two/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/behnamreyhani-masoleh/Documents/Android/NightOut/tess/tess-two/bin/AndroidManifest.xml : \
-/Users/behnamreyhani-masoleh/Documents/Android/NightOut/tess/tess-two/AndroidManifest.xml \
