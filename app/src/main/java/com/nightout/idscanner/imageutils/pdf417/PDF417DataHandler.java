d435d4153280a927bd5e2424a29f3c8d1e68b554
Behnam Reyhani-Masoleh <benreyhani@Behnams-MacBook-Pro.local>
2015-12-20 15:08:55 -0500

    added async task for handling data extraction and saving to cached file

---
+++ app/src/main/java/com/nightout/idscanner/imageutils/pdf417/PDF417DataHandler.java
@@ -0,0 +1,9 @@
+package com.nightout.idscanner.imageutils.pdf417;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by benreyhani on 15-12-20.
+ */
+public class PDF417DataHandler extends AsyncTask {
+}
